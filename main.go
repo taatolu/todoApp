ffe96c8022373dec8afd3984ae586e85ab3d20e0
yusaku <je.suis.fatigu@gmail.com>
2024-05-19 13:43:47 +0000

    feat:#2 Create usersTable

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
     "main/user"
     _ "main/app"
+    "main/app"
     
     "fmt"
     "log"
@@ -16,5 +17,13 @@ func main(){
     fmt.Println(user1)
     
     log.Println("this is a TEST")
+    
+    u := &app.User{}
+    u.Name = "test"
+    u.Email = "test@exsample.com"
+    u.Password = "testtest"
+    fmt.Println(u)
+    
+    u.CreateUser()
     
 }
