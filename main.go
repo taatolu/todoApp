7b586077df24e08eef216de841fad0857fe9ff05
yusaku <je.suis.fatigu@gmail.com>
2024-07-03 00:12:24 +0000

    feat:#17 add DeleteTodo functionaly

--- main.go
+++ main.go
@@ -12,23 +12,25 @@ import (
 
 func main(){
 
-    
-    //log.Println("this is a TEST")
-    /*
+
     u := &models.User{}
     u.Name = "testman"
     u.Email = "testman@exsample.com"
     u.Password = "testtest"
     
-    u.CreateUser()
-    */
+    //u.CreateUser()
+    
     user1, err := models.GetUser(1)
     if err != nil{
         log.Fatalln(err)
     }
     
-    user1.CreateTodo("User1のテストTODO2")
-
+    //user1.CreateTodo("テストTODO1")
+    
+    err = models.DeleteTodos(3)
+    if err != nil{
+        log.Fatalln(err)
+    }
         
     todos, err := user1.GetTodos()
     if err != nil {
