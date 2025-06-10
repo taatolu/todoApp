cd868b6c0ca4f59310c3326c9d51c53369d21a8c
Yusaku <je.suis.fatigu@gmail.com>
2025-06-10 08:27:13 +0000

    refactor: #30 test.goの配置変更

--- go.mod
+++ go.mod
@@ -3,10 +3,10 @@ module main
 go 1.13
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/go-ini/ini v1.67.0
 	github.com/google/uuid v1.6.0
 	github.com/lib/pq v1.10.9
-	github.com/stretchr/objx v0.3.0 // indirect
 	github.com/stretchr/testify v1.7.0
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- models/base_test.go
+++ models/base_test.go
@@ -1,14 +1,13 @@
-package test
+package models
 
 import(
     "testing"
     "github.com/stretchr/testify/assert"
-    "main/models"
     "main/config"
     )
 
 func TestInitDB(t *testing.T){
     cfg, err := config.LoadConfig("test")
-    err = models.InitDB(cfg)
+    err = InitDB(cfg)
     assert.NoError(t, err)
 }
