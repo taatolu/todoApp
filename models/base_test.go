10441d2ee1cb723d89ee29bfd80a27c4d49798ae
Yusaku <je.suis.fatigu@gmail.com>
2025-06-30 05:48:57 +0000

    chore: #37 add staticcheck

--- models/base_test.go
+++ models/base_test.go
@@ -8,6 +8,7 @@ import(
 
 func TestInitDB(t *testing.T){
     cfg, err := config.LoadConfig("test")
+    assert.NoError(t, err, "LoadConfigでエラー発生")
     err = InitDB(cfg)
-    assert.NoError(t, err)
+    assert.NoError(t, err, "InitDBでエラー発生")
 }
